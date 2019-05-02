66bb4dcd41c8c80597a4c1be7fd4ed9360045a12
Destiny Ajakaiye <destinyajakaiye@gmail.com>
2019-05-02 01:27:25 -0700

    Updated USSD

--- app/Controllers/Http/UssdController.js
+++ app/Controllers/Http/UssdController.js
@@ -4,12 +4,12 @@ class UssdController {
     
     async index ({request, response}) {
         const params = request.only(['sessionId', 'serviceCode', 'phoneNumber', 'text'])
-        const SESSION_CONTINUE = "CON ";
-        const SESSION_END = "END ";
-        const RESPONSE_CODE = 200;
-        let res = ''
+        const SESSION_CONTINUE = "CON "
+        const SESSION_END = "END "
+        const RESPONSE_CODE = 200
+        let res = ""
 
-        if (params.text === "") {
+        if (params.text === '') {
             res = `${SESSION_CONTINUE}Welcome to RemindMe Service
             Press
             1. Immunization
@@ -18,12 +18,12 @@ class UssdController {
             4. Confirm Immunization
             5. Unsubscribe`
 
-        } else if(params.text === "1") {
+        } else if(params.text === '1') {
             res = `${SESSION_END}Your phone number is ${params.phoneNumber}`;
         }
 
         response.header('Content-type', 'text/plain')
-        await response.status(RESPONSE_CODE).send(res)
+        response.status(RESPONSE_CODE).send(res)
     }
 }
 
